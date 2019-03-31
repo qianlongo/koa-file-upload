864cc1b90531cba1a8cd806afc87238e85defbac
qianlongo <1162633511@qq.com>
2019-03-31 11:52:29 +0800

    update: 支持多文件上传 以及修改上传最大文件尺寸为10M

--- server/controller/upload.js
+++ server/controller/upload.js
@@ -5,12 +5,16 @@ const { uploadFilesPath } = require('../config')
 module.exports = {
   async upload (ctx, next) {
     const file = ctx.request.files.file
-    const reader = fs.createReadStream(file.path)
-    const fileName = file.name
-    const filePath = `${uploadFilesPath}/${fileName}`
-    const upStream = fs.createWriteStream(filePath)
+    const upload = (file) => {
+      const reader = fs.createReadStream(file.path)
+      const fileName = file.name
+      const filePath = `${uploadFilesPath}/${fileName}`
+      const upStream = fs.createWriteStream(filePath)
 
-    reader.pipe(upStream)
+      reader.pipe(upStream)
+    }
+
+    Array.isArray(file) ? file.forEach((it) => upload(it)) : upload(file)
 
     ctx.redirect('/middle')
   }
--- server/middleware/index.js
+++ server/middleware/index.js
@@ -19,7 +19,7 @@ module.exports = (app) => {
   app.use(koaBody({
     multipart: true,
     formidable: {
-      maxFileSize: 200 * 1024 * 1024 // 设置上传文件大小最大限制，默认2M
+      maxFileSize: 1000 * 1024 * 1024 // 设置上传文件大小最大限制，默认10M
     }
   }))
   app.use(pureKoaRouter({
